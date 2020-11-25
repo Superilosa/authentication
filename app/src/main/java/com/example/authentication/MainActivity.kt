607f1b84fd541e44e22bd31b3229b07fc63eef74
Ilia Gharibashvili <ilia.gharibashvili.1@btu.edu.ge>
2020-11-25 12:53:52 +0400

    initial commit

---
+++ app/src/main/java/com/example/authentication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.authentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
